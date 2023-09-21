3004da48c862ba24f9a402383a44c2e4bf1b6e0b
Vladimir Daragan <94220585+Vladimir12241@users.noreply.github.com>
2023-09-21 10:36:20 +0300

    Update upstaff_test.js

--- upstaff_test.js
+++ upstaff_test.js
@@ -6,7 +6,7 @@ const addSpecialDiv = () => {
         const productsList = allProducts.querySelectorAll('.product-item');
         const myDiv = divCreator();
 
-        allProducts.insertBefore(myDiv, productsList[5]);
+        allProducts.insertBefore(myDiv, productsList[4]);
     };
 
     const divCreator = () => {
